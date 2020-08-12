ba247ebe51b2dc37a229891918653e1ede1f7521
ChenluJiang <chenlujiang@brandeis.edu>
2020-08-12 10:43:29 +0800

    completed leapyear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -3,6 +3,21 @@
  */
 public class LeapYear {
 
+    /** Method to check if a year is a leap year
+     * @param year to be checked
+     */
+
+     public static boolean isLeapYear(int year){
+        if(year % 400 == 0){
+            return true;
+        } else if (year % 4 == 0 & year % 100 != 0){
+            return true;
+        } else {
+            return false;
+        }
+     }
+
+
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
