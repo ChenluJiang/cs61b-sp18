eea200e4c58aa955ab9e369a4a708d369afd1fef
ChenluJiang <chenlujiang@brandeis.edu>
2020-09-21 15:11:18 +0800

    modified proj1b for TestOffByOne

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -17,6 +17,7 @@ public class TestOffByOne {
         assertFalse(offByOne.equalChars('a', 'e'));
         assertFalse(offByOne.equalChars('a', 'a'));
         assertFalse(offByOne.equalChars('z', 'a'));
+        assertFalse(offByOne.equalChars('A', 'a'));
     }
     /* Uncomment this class once you've created your CharacterComparator interface and OffByOne class. */
 }
